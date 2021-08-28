c85fc3200a5d385f56f55efb3e9d20e684dc41a7
Prabhjot Singh <Prabhx.jott@gmail.com>
2021-08-28 11:21:30 +0530

    First Commit

--- Day 1 (1-5)/Broze.cpp
+++ Day 1 (1-5)/Broze.cpp
@@ -1,10 +1,9 @@
-
+/*
 
 Ternary numeric notation is quite popular in Berland. To telegraph the ternary number the Borze alphabet is used. Digit 0 is transmitted as «.», 1 as «-.» and 2 as «--». You are to decode the Borze code, i.e. to find out the ternary number given its representation in Borze alphabet.
 
 Input
-The first line contains a number in Borze code. The length of the string is between 1 and 200 characters. It's guaranteed that the given string
-is a valid Borze code of some ternary number (this number can have leading zeroes).
+The first line contains a number in Borze code. The length of the string is between 1 and 200 characters. It's guaranteed that the given string is a valid Borze code of some ternary number (this number can have leading zeroes).
 
 Output
 Output the decoded ternary number. It can have leading zeroes.
@@ -29,6 +28,7 @@ output
 Problem Link:
 https://codeforces.com/problemset/problem/32/B
 
+*/
 
 
 
